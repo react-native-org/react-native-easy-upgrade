8c7c5768f7bce3343f87830dd0084fe46f5e65a6
Jay.M.Hu <jay.m.hu@newegg.com>
2018-05-31 16:49:36 +0800

    Update install apk and get package info.

--- android/src/main/java/com/reactlibrary/RNAppUpgradeModule.java
+++ android/src/main/java/com/reactlibrary/RNAppUpgradeModule.java
@@ -8,15 +8,46 @@ import com.facebook.react.bridge.Callback;
 
 public class RNAppUpgradeModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
+    private String versionName = "1.0.0";
+    private int versionCode = 1;
 
-  public RNAppUpgradeModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNAppUpgradeModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        try {
+            PackageInfo pInfo = reactContext.getPackageManager().getPackageInfo(reactContext.getPackageName(), 0);
+            versionName = pInfo.versionName;
+            versionCode = pInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-  @Override
-  public String getName() {
-    return "RNAppUpgrade";
-  }
+    @Override
+    public String getName() {
+        return "RNAppUpgrade";
+    }
+
+    @Override
+    public Map<String, Object> getConstrants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("versionName", versionName);
+        constants.put("versionCode", versionCode);
+        return constants;
+    }
+
+    @ReactMethod
+    public void installApk(String apkPath) {
+        String changeModCommand = "chmod 777 " + apkPath;
+        try {
+            Runtime.getRuntime().exec(cmd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setDataAndType(Uri.parse("file://" + apkPath), "application/vnd.android.package-archive");
+        this.getCurrentActivity().startActivity(intent);
+    }
 }
